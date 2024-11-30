cdf43467a28b44b1d42136cef03d8a8bb3228de1
hosternus <90009071+hosternus@users.noreply.github.com>
2024-11-30 15:14:32 +0300

    start

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+class BankAccount {};
+class Customer {};
+class Transaction {};
+
+class SavingsAccount : BankAccount {};
+class CheckingAccount : BankAccount {};
+
+
+
+
+int main(void) {
+
+
+
+    return 0;
+}
