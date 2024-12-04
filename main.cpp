924d8f49a0d5b9703efc3e490ac319f3439ac2e6
hosternus <90009071+hosternus@users.noreply.github.com>
2024-12-05 00:24:22 +0300

    vars to classes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,84 @@
 #include <iostream>
-
+#include <string>
+#include <vector>
 
 using namespace std;
 
+enum Currency {
+    USD,
+    RUB,
+    EUR,
+    AUD,
+    BYN,
+    AED,
+    CNY
+};
+
+enum TransactionType {
+    Deposit,
+    Withdrawal,
+    AnnualPercents,
+    Fee
+};
 
 
-class BankAccount {};
-class Customer {};
-class Transaction {};
 
-class SavingsAccount : BankAccount {};
-class CheckingAccount : BankAccount {};
+class BankAccount {
+
+    private:
+        size_t id;
+        Currency Currency;
+        string DateCreated;
+        float Balance;
+        bool IsActive = true;
+        vector<Transaction> TransactionsList;
+
+
+
+};
+
+class Transaction {
+    private:
+        size_t id;
+        TransactionType Type;
+        string Date;
+        float Amount;
+
+};
+
+class Customer {
+    
+    private:
+        size_t id;
+        long PhoneNumber;
+        string email;
+        string Address;
+        string Name, Surname;
+        string DateOfBirth;
+        bool IsVIP = false;
+        vector<BankAccount> Accounts;
+
+
+};
+
+
+class SavingsAccount : BankAccount {
+
+    private:
+        float Annual;
+
+};
+
+class CheckingAccount : BankAccount {
+
+    private:
+        double Fee;
+
+};
+
+
+
+
 
 
 
